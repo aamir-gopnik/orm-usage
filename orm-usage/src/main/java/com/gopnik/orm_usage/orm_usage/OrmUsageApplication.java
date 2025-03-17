a1ddb5e84eadcc007d22be307b546fbb383c34c1
aamir-gopnik <aamir.gopnik@gmail.com>
2025-03-18 00:40:45 +0530

    project setup done

---
+++ orm-usage/src/main/java/com/gopnik/orm_usage/orm_usage/OrmUsageApplication.java
@@ -0,0 +1,13 @@
+package com.gopnik.orm_usage.orm_usage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmUsageApplication.class, args);
+	}
+
+}
