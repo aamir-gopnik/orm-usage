3c2dc4e3a6074df94251bddab52f684c1d6f7504
aamir-gopnik <aamir.gopnik@gmail.com>
2025-03-18 01:00:25 +0530

    project setup done

---
+++ src/test/java/com/gopnik/orm_usage/orm_usage/OrmUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopnik.orm_usage.orm_usage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
